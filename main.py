890664f2c83b30f01e37e552eab3103317f6e8a2
Alina-is-the-best <107186145+Alina-is-the-best@users.noreply.github.com>
2024-10-09 15:43:50 +0300

    1

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import telebot
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+bot = telebot.TeleBot('7883139018:AAGaMHDoRfVT6K2V7FaGQwETVxrRlP2Wu2M')
+user_dict = {}
+
+# Словарь для хранения зарегистрированных пользователей и их кодов игр
+registered_users = {}
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#первое взаимодействие с ботом
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет, я бот для игры в мафию! Используйте команду /register, чтобы зарегистрироваться для игры.")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#промежуточный этап запроса кода игры
+@bot.message_handler(commands=['register'])
+def register_user(message):
+    msg = bot.reply_to(message, "Пожалуйста, введите номер игры для регистрации:")
+    bot.register_next_step_handler(msg, process_game_code)
+
+
+def process_game_code(message):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    game_code = message.text
+
+    # Сохраняем пользователя и код игры в словаре
+    registered_users[user_id] = {'name': user_name, 'game_code': game_code}
+
+    bot.reply_to(message, f"{user_name}, вы успешно зарегистрированы на игру с номером: {game_code}!")
+
+
+@bot.message_handler(func=lambda message: True)
+def check_message(message):
+    if 'играем' in message.text.lower():
+        send_private_messages()
+
+
+#отправка личных сообщений с ролью
+def send_private_messages():
+    for user_id, user_info in registered_users.items():
+        user_name = user_info['name']
+        try:
+            bot.send_message(user_id, f'Привет, {user_name}! Твоя роль?')
+        except Exception as e:
+            print(f"Ошибка при отправке сообщения пользователю {user_id}: {e}")
+
+
+def main():
+    bot.polling(none_stop=True)
+
+
+if __name__ == "__main__":
+    main()
